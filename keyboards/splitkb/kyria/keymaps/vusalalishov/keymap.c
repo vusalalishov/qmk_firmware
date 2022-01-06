e56aac8474fad52f631aa8db91a63bef8c3b9994
vusalalishov <vusal.alishov@gmail.com>
2022-01-07 00:43:09 +0100

    Formatted keymaps and added new ones

--- keyboards/splitkb/kyria/keymaps/vusalalishov/keymap.c
+++ keyboards/splitkb/kyria/keymaps/vusalalishov/keymap.c
@@ -2,6 +2,7 @@
 
 enum layers {
     _QWERTY = 0,
+	_PROG,
 };
 
 enum {
@@ -14,25 +15,19 @@ enum {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/*
- * Base Layer: QWERTY
- *
- * ,-------------------------------------------.                              ,-------------------------------------------.
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
- * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
- * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |CapsLk|  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Adjust| LGUI | LAlt/| Space| Nav  |  | Sym  | Space| AltGr| RGUI | Menu |
- *                        |      |      | Enter|      |      |  |      |      |      |      |      |
- *                        `----------------------------------'  `----------------------------------'
- */
+
     [_QWERTY] = LAYOUT(
-     KC_ESC  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     KC_TAB , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,TD(TD_COL_SEM),CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     KC_TRNS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                KC_TRNS , KC_LGUI, KC_LSFT, KC_ENT , KC_TRNS   ,     KC_TRNS    , KC_SPC ,KC_RALT, KC_RGUI, KC_APP
+	     KC_ESC  , KC_Q , KC_W    , KC_E    , KC_R    , KC_T                 ,                     /**/                     KC_Y                 , KC_U    , KC_I    , KC_O   , KC_P           , KC_BSPC  ,
+	     KC_TAB  , KC_A , KC_S    , KC_D    , KC_F    , KC_G                 ,                     /**/                     KC_H                 , KC_J    , KC_K    , KC_L   , TD(TD_COL_SEM) , CTL_QUOT ,
+	     KC_LSFT , KC_Z , KC_X    , KC_C    , KC_V    , KC_B                 , KC_LBRC , KC_CAPS , /**/ KC_TRNS , KC_RBRC , KC_N                 , KC_M    , KC_COMM , KC_DOT , KC_SLSH        , KC_TRNS  ,
+                          KC_TRNS , KC_TRNS , KC_TRNS , MT(MOD_LSFT, KC_ENT) , KC_LCMD           , /**/           KC_RALT , MT(MOD_RCTL, KC_SPC) , KC_TRNS , KC_TRNS , KC_APP
+    ),
+
+    [_PROG] = LAYOUT(
+	     KC_ESC  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
+	     KC_TAB , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,TD(TD_COL_SEM),CTL_QUOT,
+	     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,                     KC_TRNS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+                                KC_TRNS , KC_TRNS, KC_TRNS, MT(MOD_LSFT, KC_ENT) ,KC_LCMD       ,     KC_RALT    , MT(MOD_RCTL, KC_SPC) ,KC_TRNS, KC_TRNS, KC_APP
     ),
 
 };
