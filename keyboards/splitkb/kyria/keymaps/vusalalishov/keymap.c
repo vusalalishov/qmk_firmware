02ad4a1061a73f5f3bb6744c8cd93825e6d3fe60
vusalalishov <vusal.alishov@gmail.com>
2022-01-10 00:02:05 +0100

    Use auto-shift

--- keyboards/splitkb/kyria/keymaps/vusalalishov/keymap.c
+++ keyboards/splitkb/kyria/keymaps/vusalalishov/keymap.c
@@ -8,7 +8,11 @@ enum layers {
 
 enum {
 	TD_COL_SEM,
-	TD_QUOT_DOULE
+	TD_QUOT_DOULE,
+	TD_DOT_COMM,
+	TD_SLSH_BCKSLH,
+	TD_UNDS_MIN,
+	TD_P_PIPE
 };
 
 // Aliases for readability
@@ -19,23 +23,23 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
-	     KC_ESC  , KC_Q , KC_W    , KC_E    , KC_R    , KC_T                 ,                      /**/                                             KC_Y    , KC_U    , KC_I    , KC_O   , KC_P           , KC_BSPC            ,
-	     KC_TAB  , KC_A , KC_S    , KC_D    , KC_F    , KC_G                 ,                      /**/                                             KC_H    , KC_J    , KC_K    , KC_L   , TD(TD_COL_SEM) , TD(TD_QUOT_DOULE)  ,
-	     KC_LSFT , KC_Z , KC_X    , KC_C    , KC_V    , KC_B    , KC_LBRC              , KC_CAPS  , /**/           KC_TRNS  , KC_RBRC              , KC_N    , KC_M    , KC_COMM , KC_DOT , KC_SLSH        , KC_TRNS            ,
+	     KC_ESC  , KC_Q , KC_W    , KC_E    , KC_R    , KC_T                 ,                      /**/                                             KC_Y    , KC_U    , KC_I            , KC_O               , TD(TD_P_PIPE)      , KC_BSPC           ,
+	     KC_TAB  , KC_A , KC_S    , KC_D    , KC_F    , KC_G                 ,                      /**/                                             KC_H    , KC_J    , KC_K            , KC_L               , TD(TD_COL_SEM)     , TD(TD_QUOT_DOULE) ,
+	     KC_LSFT , KC_Z , KC_X    , KC_C    , KC_V    , KC_B    , KC_LBRC              , KC_CAPS  , /**/           KC_TRNS  , KC_RBRC              , KC_N    , KC_M    , TD(TD_DOT_COMM) , TD(TD_UNDS_MIN)    , TD(TD_SLSH_BCKSLH) , KC_TRNS           ,
                                     KC_TRNS , KC_TRNS , KC_LCMD , MT(MOD_LSFT, KC_ENT) , MO(_SYM) , /**/           MO(_NUM) , MT(MOD_RCTL, KC_SPC) , KC_RALT , KC_TRNS , KC_APP
     ),
 
     [_SYM] = LAYOUT(
-	     _______ , _______ , KC_GRV  , KC_AT   , KC_DLR  , KC_ASTR                 ,                /**/                               KC_LT   , KC_LCBR , KC_RCBR , KC_EQL  , KC_UNDS , _______ ,
-	     _______ , _______ , KC_TILD , KC_CIRC , KC_HASH , _______                 ,                /**/                               KC_GT   , KC_LPRN , KC_RPRN , KC_PLUS , KC_MINS , _______ ,
-	     _______ , _______ , KC_AMPR , KC_PIPE , KC_PERC , _______ , _______ , _______ ,            /**/           _______ , _______ , KC_EXLM , KC_LBRC , KC_RBRC , KC_QUES , KC_BSLS , _______ ,
-                                       _______ , _______ , _______ , _______ , _______ ,            /**/           _______ , _______ , _______ , _______ , _______
+	     _______ , KC_GRV  , KC_HASH , KC_AT   , KC_EXLM , _______                  ,                /**/                               _______ , KC_LCBR , KC_RCBR , KC_LT   , KC_GT   , _______ ,
+	     _______ , _______ , KC_CIRC , KC_PERC , KC_DLR  , _______                  ,                /**/                               _______ , KC_LPRN , KC_RPRN , KC_QUES , KC_PLUS , _______ ,
+	     _______ , _______ , KC_TILD , KC_ASTR , KC_AMPR , _______ , _______ , _______ ,             /**/           _______ , _______ , _______ , KC_LBRC , KC_RBRC , KC_EQL  , _______ , _______ ,
+                                       _______ , _______ , _______ , _______ , _______ ,             /**/           _______ , _______ , _______ , _______ , _______
     ),
 
     [_NUM] = LAYOUT(
-	     _______ , _______ , KC_0    , KC_3    , KC_2    , KC_1                    ,                /**/                               _______ , _______ , _______ , _______ , _______ , _______ ,
-	     _______ , _______ , _______ , KC_6    , KC_5    , KC_4                    ,                /**/                               _______ , _______ , _______ , _______ , _______ , _______ ,
-	     _______ , _______ , _______ , KC_9    , KC_8    , KC_7    , _______ , _______ ,            /**/           _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
+	     _______ , KC_0    , KC_3    , KC_2    , KC_1, _______                     ,                /**/                               _______ , _______ , _______ , _______  , _______ , _______ ,
+	     _______ , _______ , KC_6    , KC_5    , KC_4, _______                     ,                /**/                               KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT , _______ , _______ ,
+	     _______ , _______ , KC_9    , KC_8    , KC_7, _______     , _______ , _______ ,            /**/           _______ , _______ , _______ , _______ , _______ , _______  , _______ , _______ ,
                                        _______ , _______ , _______ , _______ , _______ ,            /**/           _______ , _______ , _______ , _______ , _______
     ),
 
@@ -52,4 +56,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_COL_SEM] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_COLN),
 	[TD_QUOT_DOULE] = ACTION_TAP_DANCE_DOUBLE(KC_DQUO, KC_QUOT),
+	[TD_DOT_COMM] = ACTION_TAP_DANCE_DOUBLE(KC_DOT, KC_COMM),
+	[TD_SLSH_BCKSLH] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_BSLS),
+	[TD_UNDS_MIN] = ACTION_TAP_DANCE_DOUBLE(KC_UNDS, KC_MINS),
+	[TD_P_PIPE] = ACTION_TAP_DANCE_DOUBLE(KC_P, KC_PIPE),
 };
