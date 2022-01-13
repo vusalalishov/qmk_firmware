e1b5da7317251160ace23b733a718af35790b62d
vusalalishov <vusal.alishov@gmail.com>
2022-01-13 10:49:02 +0100

    Added numpad key

--- keyboards/splitkb/kyria/keymaps/vusalalishov/keymap.c
+++ keyboards/splitkb/kyria/keymaps/vusalalishov/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
 	     KC_ESC , KC_Q , KC_W , KC_E    , KC_R         , KC_T        ,                              /**/                                      KC_Y        , KC_U        , KC_I   , KC_O    , KC_P    , KC_BSPC ,
 	     KC_TAB , KC_A , KC_S , KC_D    , KC_F         , KC_G        ,                              /**/                                      KC_H        , KC_J        , KC_K   , KC_L    , TD_COL  , TD_QUOT ,
-	     KC_P0  , KC_Z , KC_X , KC_C    , KC_V         , KC_B        , XXXXXXX     , XXXXXXX    ,   /**/           KC_PIPE    , KC_BSLS     , KC_N        , KC_M        , TD_DOT , TD_UNDS , KC_SLSH , XXXXXXX ,
+	     KC_NUM , KC_Z , KC_X , KC_C    , KC_V         , KC_B        , XXXXXXX     , XXXXXXX    ,   /**/           KC_PIPE    , KC_BSLS     , KC_N        , KC_M        , TD_DOT , TD_UNDS , KC_SLSH , XXXXXXX ,
                                 XXXXXXX , MT_LALT_LBRC , MT_LCMD_PRN , MT_LSFT_CRB , LT_DEV_ENT ,   /**/           LT_NUM_SPC , MT_RSFT_CBR , MT_RCTL_PRN , MT_RALT_BRC , XXXXXXX
     ),
 
