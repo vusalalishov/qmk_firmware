e379e0795709a34e84f0a5255581afcacdcf6df8
Vusal Alishov <vusal.alishov@gmail.com>
2022-01-31 22:16:56 +0100

    Added sym layer

--- keyboards/splitkb/kyria/keymaps/vusalalishov/keymap.c
+++ keyboards/splitkb/kyria/keymaps/vusalalishov/keymap.c
@@ -2,28 +2,30 @@
 
 enum layers {
     _QWERTY = 0,
+	_WORKMAN,
 	_DEV, 
 	_NUM,
+	_SYM,
 };
 
 enum {
-	TD_COL_SEM,
 	TD_QUOT_DOULE,
 	TD_DOT_COMM,
 	TD_UNDS_MIN,
 };
 
 // Aliases for readability
-#define QWERTY       DF(_QWERTY)
+#define QWERTY        DF(_QWERTY)
+#define WORKMAN       DF(_WORKMAN)
 
+#define MT_A_LSFT    MT(MOD_LSFT, KC_A)
+#define MT_SCLN_RSFT MT(MOD_RSFT, KC_SCLN)
 #define MT_CMD_SPC   MT(MOD_LGUI, KC_SPC)
-#define MT_SFT_ENT   MT(MOD_LSFT, KC_ENT)
 
 #define MO_DEV       MO(_DEV)
-#define MO_NUM       MO(_NUM)
 
-#define MT_ALT_PRN   MT(MOD_LALT, KC_LPRN)
-#define MT_CTRL_PRN  MT(MOD_LCTL, KC_RPRN)
+#define LT_NUM_ENT       LT(_NUM, KC_ENT)
+#define LT_SYM_F         LT(_SYM, KC_F)
 
 // Tap dance
 #define TD_DOT       TD(TD_DOT_COMM)
@@ -35,16 +37,23 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
-	     KC_ESC  , KC_Q , KC_W , KC_E    , KC_R    , KC_T    ,                           /**/                            KC_Y    , KC_U    , KC_I   , KC_O    , KC_P    , KC_BSPC ,
-	     KC_TAB  , KC_A , KC_S , KC_D    , KC_F    , KC_G    ,                           /**/                            KC_H    , KC_J    , KC_K   , KC_L    , TD_COL  , TD_QUOT ,
-	     KC_RGUI , KC_Z , KC_X , KC_C    , KC_V    , KC_B    , MO_DEV     , KC_LCBR    , /**/ KC_RCBR , MO_NUM         , KC_N    , KC_M    , TD_DOT , TD_UNDS , KC_SLSH , XXXXXXX ,
-                                 XXXXXXX , XXXXXXX , KC_LBRC , MT_CMD_SPC , MT_ALT_PRN , /**/ MT_CTRL_PRN , MT_SFT_ENT , KC_RBRC , XXXXXXX , XXXXXXX
+	     KC_ESC  , KC_Q      , KC_W , KC_E    , KC_R     , KC_T    ,                      /**/                            KC_Y    , KC_U    , KC_I   , KC_O    , KC_P          , KC_BSPC ,
+	     KC_TAB  , MT_A_LSFT , KC_S , KC_D    , LT_SYM_F , KC_G    ,                      /**/                            KC_H    , KC_J    , KC_K   , KC_L    , MT_SCLN_RSFT  , TD_QUOT ,
+	     KC_RGUI , KC_Z      , KC_X , KC_C    , KC_V     , KC_B    , XXXXXXX  , XXXXXXX , /**/ XXXXXXX    , XXXXXXX     , KC_N    , KC_M    , TD_DOT , TD_UNDS , KC_SLSH       , XXXXXXX ,
+                                      XXXXXXX , XXXXXXX  , KC_LALT , KC_LCTRL , MO_DEV  , /**/ LT_NUM_ENT , MT_CMD_SPC  , XXXXXXX , XXXXXXX , WORKMAN
+    ),
+
+    [_WORKMAN] = LAYOUT(
+	     _______ , KC_Q , KC_D , KC_R    , KC_W    , KC_B    ,                     /**/                     KC_J    , KC_F    , KC_U    , KC_P    , KC_SCLN , KC_BSPC ,
+	     _______ , KC_A , KC_S , KC_H    , KC_T    , KC_G    ,                     /**/                     KC_Y    , KC_N    , KC_E    , KC_O    , KC_I    , KC_QUOT ,
+	     _______ , KC_Z , KC_X , KC_M    , KC_C    , KC_V    , _______ , _______ , /**/ _______ , _______ , KC_K    , KC_L    , _______ , _______ , _______ , _______ ,
+                                 _______ , _______ , _______ , _______ , _______ , /**/ _______ , _______ , _______ , _______ , QWERTY
     ),
 
     [_DEV] = LAYOUT(
-	     XXXXXXX , XXXXXXX , KC_HASH , KC_AT   , KC_EXLM , XXXXXXX ,                     /**/                     XXXXXXX , KC_LT   , KC_GT   , KC_GRV  , XXXXXXX , _______ ,
-	     XXXXXXX , XXXXXXX , KC_CIRC , KC_PERC , KC_DLR  , XXXXXXX ,                     /**/                     XXXXXXX , KC_QUES , KC_PLUS , KC_EQL  , XXXXXXX , XXXXXXX ,
-	     XXXXXXX , XXXXXXX , KC_TILD , KC_ASTR , KC_AMPR , XXXXXXX , XXXXXXX , XXXXXXX , /**/ XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_PIPE , KC_BSLS , XXXXXXX , XXXXXXX ,
+	     XXXXXXX , XXXXXXX , KC_HASH , KC_AT   , KC_EXLM , XXXXXXX ,                     /**/                     KC_PIPE , KC_LT   , KC_GT   , KC_GRV  , XXXXXXX , _______ ,
+	     XXXXXXX , KC_TILD , KC_CIRC , KC_PERC , KC_DLR  , XXXXXXX ,                     /**/                     KC_BSLS , KC_QUES , KC_PLUS , KC_EQL  , XXXXXXX , XXXXXXX ,
+	     XXXXXXX , XXXXXXX , XXXXXXX , KC_ASTR , KC_AMPR , XXXXXXX , XXXXXXX , XXXXXXX , /**/ XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
                                        XXXXXXX , _______ , _______ , _______ , _______ , /**/ _______ , _______ , _______ , _______ , XXXXXXX
     ),
 
@@ -55,6 +64,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        XXXXXXX , _______ , _______ , _______ , _______ , /**/ _______ , _______ , _______ , _______ , XXXXXXX
     ),
 
+    [_SYM] = LAYOUT(
+	     _______ , _______ , _______ , _______ , _______ , _______ ,                     /**/                     KC_LPRN , KC_LCBR           , KC_RCBR        , _______    , _______ , _______ ,
+	     _______ , _______ , _______ , _______ , _______ , _______ ,                     /**/                     KC_RPRN , LGUI(KC_LEFT)     , LGUI(KC_RIGHT) , _______    , KC_COLN , _______ ,
+	     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , /**/ _______ , _______ , _______ , KC_LBRC           , KC_RBRC        , _______    , _______ , _______ ,
+                                       _______ , _______ , _______ , _______ , _______ , /**/ _______ , _______ , _______ , _______           , _______
+    ),
 
     // [_LAYER] = LAYOUT(
 	//      _______ , _______ , _______ , _______ , _______ , _______ ,                     /**/           _______ , _______ , _______ , _______ , _______ , _______ ,
@@ -66,29 +81,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Tap dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_COL_SEM] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_COLN),
 	[TD_QUOT_DOULE] = ACTION_TAP_DANCE_DOUBLE(KC_DQUO, KC_QUOT),
 	[TD_DOT_COMM] = ACTION_TAP_DANCE_DOUBLE(KC_DOT, KC_COMM),
 	[TD_UNDS_MIN] = ACTION_TAP_DANCE_DOUBLE(KC_UNDS, KC_MINS),
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	switch (keycode) {
-		case LALT_T(KC_LPRN):
-			if (record->tap.count && record->event.pressed) {
-				tap_code16(KC_LPRN);
-				return false;
-			}
-			break;
-		case LCTL_T(KC_RPRN):
-			if (record->tap.count && record->event.pressed) {
-				tap_code16(KC_RPRN);
-				return false;
-			}
-			break;
-		default:
-			return true;
-	}
-	return true;
-}
 
