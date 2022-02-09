b493a631a67d46c0caaf116ba8fb71719e342815
Vusal Alishov <vusal.alishov@gmail.com>
2022-02-09 14:16:49 +0100

    Added workman layer

--- keyboards/splitkb/kyria/keymaps/vusalalishov/keymap.c
+++ keyboards/splitkb/kyria/keymaps/vusalalishov/keymap.c
@@ -10,8 +10,6 @@ enum layers {
 
 enum {
 	TD_QUOT_DOULE,
-	TD_DOT_COMM,
-	TD_UNDS_MIN,
 };
 
 // Aliases for readability
@@ -28,18 +26,15 @@ enum {
 #define LT_SYM_F         LT(_SYM, KC_F)
 
 // Tap dance
-#define TD_DOT       TD(TD_DOT_COMM)
-#define TD_UNDS      TD(TD_UNDS_MIN)
-#define TD_COL       TD(TD_COL_SEM)
 #define TD_QUOT      TD(TD_QUOT_DOULE)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
-	     KC_ESC  , KC_Q      , KC_W , KC_E    , KC_R     , KC_T    ,                      /**/                            KC_Y    , KC_U    , KC_I   , KC_O    , KC_P          , KC_BSPC ,
-	     KC_TAB  , MT_A_LSFT , KC_S , KC_D    , LT_SYM_F , KC_G    ,                      /**/                            KC_H    , KC_J    , KC_K   , KC_L    , MT_SCLN_RSFT  , TD_QUOT ,
-	     KC_RGUI , KC_Z      , KC_X , KC_C    , KC_V     , KC_B    , XXXXXXX  , XXXXXXX , /**/ XXXXXXX    , XXXXXXX     , KC_N    , KC_M    , TD_DOT , TD_UNDS , KC_SLSH       , XXXXXXX ,
+	     KC_ESC  , KC_Q      , KC_W , KC_E    , KC_R     , KC_T    ,                      /**/                            KC_Y    , KC_U    , KC_I   , KC_O    , KC_P         , KC_BSPC ,
+	     KC_TAB  , MT_A_LSFT , KC_S , KC_D    , LT_SYM_F , KC_G    ,                      /**/                            KC_H    , KC_J    , KC_K   , KC_L    , MT_SCLN_RSFT , TD_QUOT ,
+	     KC_RGUI , KC_Z      , KC_X , KC_C    , KC_V     , KC_B    , XXXXXXX  , XXXXXXX , /**/ XXXXXXX    , XXXXXXX     , KC_N    , KC_M    , KC_DOT , KC_UNDS , _______      , XXXXXXX ,
                                       XXXXXXX , XXXXXXX  , KC_LALT , KC_LCTRL , MO_DEV  , /**/ LT_NUM_ENT , MT_CMD_SPC  , XXXXXXX , XXXXXXX , WORKMAN
     ),
 
@@ -65,10 +60,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_SYM] = LAYOUT(
-	     _______ , _______ , _______ , _______ , _______ , _______ ,                     /**/                     KC_LPRN , KC_LCBR           , KC_RCBR        , _______    , _______ , _______ ,
-	     _______ , _______ , _______ , _______ , _______ , _______ ,                     /**/                     KC_RPRN , LGUI(KC_LEFT)     , LGUI(KC_RIGHT) , _______    , KC_COLN , _______ ,
-	     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , /**/ _______ , _______ , _______ , KC_LBRC           , KC_RBRC        , _______    , _______ , _______ ,
-                                       _______ , _______ , _______ , _______ , _______ , /**/ _______ , _______ , _______ , _______           , _______
+	     _______ , _______ , _______ , _______ , _______ , _______ ,                     /**/                     LGUI(KC_LEFT)  , KC_LCBR , KC_RCBR , KC_MINS , KC_SLSH , _______ ,
+	     _______ , _______ , _______ , _______ , _______ , _______ ,                     /**/                     LGUI(KC_RIGHT) , KC_LPRN , KC_RPRN , KC_COMM , KC_COLN , _______ ,
+	     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , /**/ _______ , _______ , _______        , KC_LBRC , KC_RBRC , _______ , _______ , _______ ,
+                                       _______ , _______ , _______ , _______ , _______ , /**/ _______ , _______ , _______        , _______ , _______
     ),
 
     // [_LAYER] = LAYOUT(
@@ -82,7 +77,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Tap dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
 	[TD_QUOT_DOULE] = ACTION_TAP_DANCE_DOUBLE(KC_DQUO, KC_QUOT),
-	[TD_DOT_COMM] = ACTION_TAP_DANCE_DOUBLE(KC_DOT, KC_COMM),
-	[TD_UNDS_MIN] = ACTION_TAP_DANCE_DOUBLE(KC_UNDS, KC_MINS),
 };
 
